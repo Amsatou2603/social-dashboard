2fe0f615c2d474c843ea61a6ad453d77665b09b2
Amsatou Ndiaye <ndiayeamsatou26@gmail.com>
2026-04-27 19:40:58 +0000

    🔗 Update API URL for production

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://TON-BACKEND.onrender.com/api'
+  apiUrl: 'https://social-dashboard-api-didf.onrender.com/api'
   // ⚠️ Remplace TON-BACKEND par l'URL de ton service Render après déploiement
 };
